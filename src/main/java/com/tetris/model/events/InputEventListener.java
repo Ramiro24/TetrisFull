8b494747cccdbb69fa3ce4fa8d20820ec0a57235
matiasgarciagit <matias.garcia@mi.unc.edu.ar>
2020-06-20 23:13:47 -0300

    proyect update

---
+++ src/main/java/com/tetris/model/events/InputEventListener.java
@@ -0,0 +1,18 @@
+package com.tetris.model.events;
+
+
+import com.tetris.model.logic.ViewData;
+import com.tetris.model.logic.DownData;
+
+public interface InputEventListener {
+
+    DownData onDownEvent(MoveEvent event);
+
+    ViewData onLeftEvent(MoveEvent event);
+
+    ViewData onRightEvent(MoveEvent event);
+
+    ViewData onRotateEvent(MoveEvent event);
+
+    void createNewGame();
+}
