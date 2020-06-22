7ff8298661b51500a721acd9c0b99c5b1ea707fe
Ramiro24 <lalocarpio36@gmail.com>
2020-06-22 02:16:32 +0200

    refactorizacion y ordenado de main

--- src/main/java/com/tetris/app/Main.java
+++ src/main/java/com/tetris/app/Main.java
@@ -16,94 +16,75 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Main extends Application {
-
-	private int Dificultad;
 	
-    @Override
-    public void start(Stage StageMenu) throws Exception {
-   
-    	 ///////////////////////////////////////////////INTERFAZ MENU////////////////////////////////////////////////////
-       
+	 public static void main(String[] args) {
+	        launch(args);
+	    }
+	
+	
+	 @Override
+    public void start(Stage StageMenu) throws Exception { //comienza con stage de menu
+    	
+    	//Archivos FXML 
     	URL locationMenu = getClass().getClassLoader().getResource("Menu.fxml");
+    	URL locationTetris = getClass().getClassLoader().getResource("gameLayout.fxml");
+        URL locationGrafica = getClass().getClassLoader().getResource("NuevaVentana.fxml");
+        URL locationAyuda = getClass().getClassLoader().getResource("Ayuda.fxml");
         ResourceBundle resources = null; //recurso de lenguaje null para todos
-      
-        FXMLLoader fxmlLoaderMenu = new FXMLLoader(locationMenu, resources);
-        Parent rootMenu = fxmlLoaderMenu.load();
-        GuiMenu menu = fxmlLoaderMenu.getController();
-
-        StageMenu.setTitle("Menu");
         
+        //objetos FXML
+        FXMLLoader fxmlLoaderMenu = new FXMLLoader(locationMenu, resources);
+        FXMLLoader fxmlLoaderTetris = new FXMLLoader(locationTetris, resources);
+        FXMLLoader fxmlLoaderGrafica = new FXMLLoader(locationGrafica, resources);
+        FXMLLoader fxmlLoaderAyuda = new FXMLLoader(locationAyuda, resources);
+        
+        //Root
+        Parent rootMenu = fxmlLoaderMenu.load();
+        Parent rootTetris = fxmlLoaderTetris.load();
+        Parent rootGrafica = fxmlLoaderGrafica.load();    
+        Parent rootAyuda = fxmlLoaderAyuda.load();
+        
+        //Controladores de Fxml
+        GuiMenu menu = fxmlLoaderMenu.getController();
+        GuiController c = fxmlLoaderTetris.getController();                      
+        GuiStatsObservador s = fxmlLoaderGrafica.getController();                   
+        GuiAyuda ayuda = fxmlLoaderAyuda.getController();
+        
+        ///////////////////////////////////////////////INTERFAZ MENU////////////////////////////////////////////////////////
+       
+        StageMenu.setTitle("Menu");
         Scene sceneMenu = new Scene(rootMenu, 620, 450);
         StageMenu.setScene(sceneMenu);
-       
         StageMenu.show();
        	
-       ////////////////////////////////////////////////INTERFAZ DE TETRIS/////////////////////////////////////////////////
+       ////////////////////////////////////////////////INTERFAZ DE TETRIS////////////////////////////////////////////////////
        
-        URL locationTetris = getClass().getClassLoader().getResource("gameLayout.fxml");
-     
-        FXMLLoader fxmlLoaderTetris = new FXMLLoader(locationTetris, resources); //creo objeto FXML en java
-        Parent rootTetris = fxmlLoaderTetris.load();                             //cargo FXML como raiz
-        GuiController c = fxmlLoaderTetris.getController();                      //creo controlador de tipo gui (tetris)
-       
-        Stage StageTetris = new Stage();                                         //Stage de tetris
+        Stage StageTetris = new Stage();                                       
         StageTetris.setTitle("TetrisFULL");
-       
-        Scene SceneTetris = new Scene(rootTetris, 800, 610);                     //cargo raiz en scene
-        StageTetris.setScene(SceneTetris);                                       //cargo scene en stage
-        //primaryStage.show();
-      //game es el sujeto a ser observado
-        
-      //  c.setDificultad(Dificultad);
-    //    GameController ControladorTetris = new GameController(c);                   //creo controlador de juego y le paso controlador grafico de tetris
-        
-        //-----------------------------------------------------
-       //se lo envio a StatisticController para que lo observe
-
-     //   new StatisticController(game);
- ///////////////////////////////////////////////INTERFAZ GRAFICA (Observador)//////////////////////////////////////////////
-    
-      URL locationGrafica = getClass().getClassLoader().getResource("NuevaVentana.fxml");
-    
-        FXMLLoader fxmlLoaderGrafica = new FXMLLoader(locationGrafica, resources);  //creo objeto FXML en java
-        Parent rootGrafica = fxmlLoaderGrafica.load();                              //cargo FXML como raiz
-        GuiStatsObservador s = fxmlLoaderGrafica.getController();                   //lo cargo en controlador de tipo estadistica
-       
-        Stage StageGrafico = new Stage();                                           //Stage de grafico
+        Scene SceneTetris = new Scene(rootTetris, 800, 610);                    
+        StageTetris.setScene(SceneTetris);                                       
+      
+       //////////////////////////////////////////////INTERFAZ GRAFICA (Observador)////////////////////////////////////////////
+   
+        Stage StageGrafico = new Stage();                                         
         StageGrafico.setTitle("Grafico de estadistica Tetris");
-        
-        Scene sceneGrafica = new Scene(rootGrafica, 620, 450);                      //cargo root en scene
-        StageGrafico.setScene(sceneGrafica);                                        //cargo scene en stage
+        Scene sceneGrafica = new Scene(rootGrafica, 620, 450);                   
+        StageGrafico.setScene(sceneGrafica);                                       
      
-       c.Grafico(StageGrafico);
-       DatosObservados statObservador = new DatosObservados();                           //mi clase que implementa Subject
-       GuiStatsObservador ObservadorDisplay = new GuiStatsObservador(statObservador, s); // le paso referencia a OBSERVADOR de la clase a observar y le paso su controlador para que lo agregue a la lista de observadores del observador 
-    
- 
-       GameController ControladorTetris = new GameController(c,statObservador);            
-       //game.setEstado(10);
- 
-        
-   ////////////////////////////////////////////////////INTERFAZ AYUDA///////////////////////////////////////////////////////////
+	    c.Grafico(StageGrafico);
+	    DatosObservados statObservador = new DatosObservados();                           
+	    GuiStatsObservador ObservadorDisplay = new GuiStatsObservador(statObservador, s); // le paso referencia a OBSERVADOR de la clase a observar y le paso su controlador para que lo agregue a la lista de observadores del observador 
+	    GameController ControladorTetris = new GameController(c,statObservador);            
+      
+       ////////////////////////////////////////////////////INTERFAZ AYUDA/////////////////////////////////////////////////////
      
-      URL location3 = getClass().getClassLoader().getResource("Ayuda.fxml");
-       
-        FXMLLoader fxmlLoader3 = new FXMLLoader(location3, resources);
-        Parent root3 = fxmlLoader3.load();
-        GuiAyuda ayuda = fxmlLoader3.getController();
-        Stage help = new Stage();
-       
-       // Stage stage1 = new Stage();
-        help.setTitle("Menu");
-        help.setScene(new Scene(root3, 620, 450));
+        Stage Stagehelp = new Stage();
+        Stagehelp.setTitle("Ayuda");
+        Stagehelp.setScene(new Scene(rootAyuda, 620, 450));
     
-   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+       //////////////////////////////////////////////////////INICIO MENU//////////////////////////////////////////////////////////
         
-        menu.GUI(c , StageMenu ,StageTetris, help);
-     //   menu.Game(game);
-}
-
- public static void main(String[] args) {
-        launch(args);
+        menu.GUI(c , StageMenu ,StageTetris, Stagehelp); // inicio menu con los stage que puede iniciar y el controller tetris
     }
+
 }
