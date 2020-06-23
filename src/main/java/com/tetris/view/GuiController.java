b3dce8acb4e64713795a7d92207b694e7cb51bde
PabloA05 <pablo.cazon@alumnos.unc.edu.ar>
2020-06-23 12:09:26 -0300

    fix vel

--- src/main/java/com/tetris/view/GuiController.java
+++ src/main/java/com/tetris/view/GuiController.java
@@ -398,8 +398,10 @@ public class GuiController implements Initializable {
             timeLine.play();
         }*/
 
-        if (isPause.getValue() == Boolean.FALSE && isGameOver.getValue() == Boolean.FALSE){}
-        more.changeVelocity(this);
+        if (isPause.getValue() == Boolean.FALSE && isGameOver.getValue() == Boolean.FALSE){}{
+            more.changeVelocity(this);
+        }
+
     }
 
     public Timeline getTimeLine() {
